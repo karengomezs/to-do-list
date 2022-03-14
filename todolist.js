d197598757a097ceedeab35d9387076f1ef7e16c
karengomezs <karengomezsoto1@gmail.com>
2022-03-14 11:41:48 -0500

    number of task OK - js

--- todolist.js
+++ todolist.js
@@ -14,29 +14,50 @@ function addTask() {
     var value = enterUser.value;
     var list = document.querySelector(".list");
     var isValueEmpty = value === "";
+
+
     if (isValueEmpty) {
         return alert("please, add a new note");
     }
     list.innerHTML += `<li class="toDo"> <input type="checkbox" class="check"> <span class="text">${value}</span> </li>`;
+
+    enterUser.value = "";
+
     attacheListener();
 }
 
+
 function attacheListener() {
+
+
     var toDos = document.querySelectorAll(".toDo");
+    var numberOf = document.querySelector(".number-of");
+    var attache = document.querySelectorAll(".done")
+    var numberOfNotes = toDos.length;
+    var taskOk = attache.length;
+    numberOf.innerHTML = `${taskOk} <p> / </p> ${numberOfNotes}`
+    numberOf.innerHTML = `${numberOfNotes}`;
+
     for (var toDo of toDos) {
 
         toDo.addEventListener("click", handleClick);
     }
 }
 
+
 function handleClick(e) {
     var isInput = e.target.nodeName === "INPUT";
-
 
     if (isInput) {
         var sibling = e.target.nextElementSibling;
 
         sibling.classList.toggle("done");
-
     }
+    var toDos = document.querySelectorAll(".toDo");
+    var numberOf = document.querySelector(".number-of");
+    var attache = document.querySelectorAll(".done")
+    var numberOfNotes = toDos.length;
+    var taskOk = attache.length;
+    numberOf.innerHTML = `${taskOk} <p> / </p> ${numberOfNotes}`
+
 }
